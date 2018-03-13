ee653afde9ff297426c4da050f51187a721b8bbd
Sarah Colby <scolby@austin.utexas.edu>
2018-03-13 16:09:34 -0500

    invitation app urls

---
+++ invitations/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.events, name='events'),
+    url(r'^event/(?P<pk>\d+)/$', views.event, name='event'),
+    url(r'^event/new/$', views.event_new, name='event_new'),
+    url(r'^event/(?P<pk>\d+)/edit/$', views.event_edit, name='event_edit'),
+    url(r'^event/(?P<pk>\d+)/destroy/$', views.event_destroy, name='event_destroy'),
+]
