66d45421e898f4d0d70d4234cf3cdcaa4c64daa7
Sarah Colby <scolby@austin.utexas.edu>
2018-03-13 11:37:50 -0500

    models

--- invitations/models.py
+++ invitations/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class AppUser(AbstractUser):
+    birth_date = models.DateField()
+    invited_events = models.ManyToManyField(
+        Event,
+        through='Rsvp',
+        through_fields=('invitee', 'event'),
+    )
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    address = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    creator = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    invitees = models.ManyToManyField(
+        AppUser,
+        through='Rsvp',
+        through_fields=('event', 'invitee'),
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class Rsvp(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    invitee = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    is_attending = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
