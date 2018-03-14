b31392ad73e0e2d9a37c69b2bfaae63b2de23a29
Sarah Colby <scolby@austin.utexas.edu>
2018-03-14 16:21:52 -0500

    login required for certain pages

--- invitations/views.py
+++ invitations/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
+from .models import Event
+# from .forms import EventForm
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     pass
@@ -11,11 +14,14 @@ def events(request):
 def event(request, pk):
     pass
 
+@login_required
 def event_new(request):
     pass
 
+@login_required
 def event_edit(request, pk):
     pass
 
+@login_required
 def event_destroy(request, pk):
     pass
