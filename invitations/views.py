6806f523710111ab926bcb7d20c4e96fdec56617
Sarah Colby <scolby@austin.utexas.edu>
2018-03-14 15:39:05 -0500

    urls, views for user registration

--- invitations/views.py
+++ invitations/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def register(request):
+    pass
+
+def events(request):
+    pass
+
+def event(request, pk):
+    pass
+
+def event_new(request):
+    pass
+
+def event_edit(request, pk):
+    pass
+
+def event_destroy(request, pk):
+    pass
