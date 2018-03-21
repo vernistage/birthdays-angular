412c7c347550b358d197c90234124e2e71131a02
Sarah Colby <scolby@austin.utexas.edu>
2018-03-21 12:48:18 -0500

    looks like registration form works

--- invitations/forms.py
+++ invitations/forms.py
@@ -1,13 +1,14 @@
-# https://simpleisbetterthancomplex.com/tutorial/2017/02/18/how-to-create-user-sign-up-view.html
-from django import formsfrom django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from .models import AppUser
 
-class SignUpForm(UserCreationgForm):
+class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=100, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=100, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Input a valid email address.')
-    birth_date = models.DateField(required=True, help_text='Required. Input your birthday.')
+    birth_date = forms.DateField(required=True, help_text='Required. Input your birthday.')
 
     class Meta:
-        model = User
+        model = AppUser
         fields = ('username', 'first_name', 'last_name', 'email', 'birth_date', 'password1', 'password2', )
--- invitations/views.py
+++ invitations/views.py
@@ -1,26 +1,28 @@
-from django.contrib.auth import login, authenticate, get_user_model
-from django.contrib.auth.forms import UserCreationForm
 from .models import Event
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import login, authenticate, get_user_model
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
-AppUser = get_user_model()
+from .forms import SignUpForm
+
+User = get_user_model()
 
 def welcome(request):
     return render(request, 'welcome.html')
 
 def register(request):
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            AppUser = authenticate(username=username, password=raw_password)
-            login (request, AppUser)
-            return redirect('home')
+            user = authenticate(username=username, password=raw_password)
+            login (request, user)
+            return redirect('/')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'register.html', {'form': form})
 
 @login_required
