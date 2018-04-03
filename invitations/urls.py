5891f02ffa1da6de15cf59ccca8b982c3188e0ee
Sarah Colby <scolby@austin.utexas.edu>
2018-04-03 09:38:20 -0500

    import and implement View

--- invitations/urls.py
+++ invitations/urls.py
@@ -1,12 +1,13 @@
-from django.conf.urls import url
 from . import views
+from django.conf.urls import url
+from invitations.views import EventView, RsvpView
 
 urlpatterns = [
     url(r'^$', views.welcome, name='welcome'),
     url(r'^user/(?P<pk>\d+)/$', views.user_profile, name='user_profile'),
-    url(r'^event/(?P<pk>\d+)/$', views.event, name='event'),
-    url(r'^event/new/$', views.event_new, name='event_new'),
-    url(r'^event/(?P<pk>\d+)/edit/$', views.event_edit, name='event_edit'),
-    url(r'^event/(?P<pk>\d+)/destroy/$', views.event_destroy, name='event_destroy'),
-    url(r'^rsvp/(?P<pk>\d+)/edit/$', views.rsvp_edit, name='rsvp_edit'),
+    url(r'^event/(?P<pk>\d+)/$', views.EventView.as_view(), name='event'),
+    url(r'^event/new/$', views.EventView.as_view(), name='event_new'),
+    url(r'^event/(?P<pk>\d+)/edit/$', views.EventView.as_view(), name='event_edit'),
+    url(r'^event/(?P<pk>\d+)/destroy/$', views.EventView.as_view(), name='event_destroy'),
+    url(r'^rsvp/(?P<pk>\d+)/edit/$', views.RsvpView.as_view(), name='rsvp_edit'),
 ]
--- invitations/views.py
+++ invitations/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import login, authenticate, get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.views import View
 
 from .forms import SignUpForm
 
@@ -37,70 +39,74 @@ def user_profile(request, pk):
         to_rsvps = Rsvp.objects.filter(invitee=request.user)
     return render(request, 'users/user_profile.html', {'events': events, 'event_count': event_count, 'invitations': invitations, 'invitation_count': invitation_count, 'to_rsvps': to_rsvps})
 
-@login_required
-def event(request, pk):
-    event = Event.objects.get(pk=pk)
-    invitation_count = event.invitees.all().count()
-    invitees = event.invitees.all()
-    confirmed_yes = Rsvp.objects.all().filter(event=event, is_attending=True)
-    user = request.user
-    creator = event.is_creator(user)
-    invitee_rsvp = None
-    for invitee in invitees:
-        if invitee.pk == user.pk:
-            invitee_rsvp = Rsvp.objects.get(invitee=request.user, event=event)
-    return render(request, 'events/event.html', {'event': event, 'invitation_count': invitation_count, 'invitees': invitees, 'is_creator': creator, 'invitee_rsvp': invitee_rsvp, 'confirmed_yes': confirmed_yes})
+class EventView(View):
+    @login_required
+    def get(self, request):
+        return HttpResponse("Hello World!")
+    # def event(request, pk):
+    #     event = Event.objects.get(pk=pk)
+    #     invitation_count = event.invitees.all().count()
+    #     invitees = event.invitees.all()
+    #     confirmed_yes = Rsvp.objects.all().filter(event=event, is_attending=True)
+    #     user = request.user
+    #     creator = event.is_creator(user)
+    #     invitee_rsvp = None
+    #     for invitee in invitees:
+    #         if invitee.pk == user.pk:
+    #             invitee_rsvp = Rsvp.objects.get(invitee=request.user, event=event)
+    #     return render(request, 'events/event.html', {'event': event, 'invitation_count': invitation_count, 'invitees': invitees, 'is_creator': creator, 'invitee_rsvp': invitee_rsvp, 'confirmed_yes': confirmed_yes})
+    #
+    # @login_required
+    # def event_new(request):
+    #     if request.method == 'POST':
+    #         form = EventForm(request.POST)
+    #         invitees_keys = form['invitees'].value()
+    #         user = request.user
+    #         if form.is_valid():
+    #             new_event = form.save(commit=False)
+    #             new_event.creator = request.user
+    #             new_event.save()
+    #             new_event.invite_people(invitees_keys)
+    #             return redirect('event', pk=new_event.pk)
+    #     else:
+    #         form = EventForm()
+    #         return render(request, 'events/event_edit.html', {'form': form})
+    #
+    # @login_required
+    # def event_edit(request, pk):
+    #     event = get_object_or_404(Event, pk=pk)
+    #     current_rsvps = Rsvp.objects.all().filter(event=event)
+    #     if request.method == "POST":
+    #         form = EventForm(request.POST, instance=event)
+    #         invitees_keys = form['invitees'].value()
+    #         if form.is_valid():
+    #             event = form.save(commit=False)
+    #             event.creator = request.user
+    #             # event.disinvite(current_rsvps, invitees_keys)
+    #             event.add_invites(current_rsvps, invitees_keys)
+    #             event.save()
+    #             return redirect('event', pk=event.pk)
+    #     else:
+    #         form = EventForm(instance=event)
+    #         return render(request, 'events/event_edit.html', {'form': form})
+    #
+    # @login_required
+    # def event_destroy(request, pk):
+    #     event = get_object_or_404(Event, pk=pk)
+    #     event.delete()
+    #     return redirect('user_profile', request.user.pk)
 
-@login_required
-def event_new(request):
-    if request.method == 'POST':
-        form = EventForm(request.POST)
-        invitees_keys = form['invitees'].value()
-        user = request.user
-        if form.is_valid():
-            new_event = form.save(commit=False)
-            new_event.creator = request.user
-            new_event.save()
-            new_event.invite_people(invitees_keys)
-            return redirect('event', pk=new_event.pk)
-    else:
-        form = EventForm()
-        return render(request, 'events/event_edit.html', {'form': form})
-
-@login_required
-def event_edit(request, pk):
-    event = get_object_or_404(Event, pk=pk)
-    current_rsvps = Rsvp.objects.all().filter(event=event)
-    if request.method == "POST":
-        form = EventForm(request.POST, instance=event)
-        invitees_keys = form['invitees'].value()
-        if form.is_valid():
-            event = form.save(commit=False)
-            event.creator = request.user
-            # event.disinvite(current_rsvps, invitees_keys)
-            event.add_invites(current_rsvps, invitees_keys)
-            event.save()
-            return redirect('event', pk=event.pk)
-    else:
-        form = EventForm(instance=event)
-        return render(request, 'events/event_edit.html', {'form': form})
-
-@login_required
-def event_destroy(request, pk):
-    event = get_object_or_404(Event, pk=pk)
-    event.delete()
-    return redirect('user_profile', request.user.pk)
-
-@login_required
-def rsvp_edit(request, pk):
-    rsvp = get_object_or_404(Rsvp, pk=pk)
-    event = rsvp.event
-    if request.method == "POST":
-        form = RsvpEditForm(request.POST, instance=rsvp)
-        if form.is_valid():
-            rsvp_update = form.save(commit=False)
-            rsvp_update.save()
-            return redirect('event', pk=event.pk)
-    else:
-        form = RsvpEditForm(instance=rsvp)
-        return render(request, 'events/rsvp_edit.html', {'form': form, 'event':event})
+class RsvpView(View):
+    @login_required
+    def rsvp_edit(request, pk):
+        rsvp = get_object_or_404(Rsvp, pk=pk)
+        event = rsvp.event
+        if request.method == "POST":
+            form = RsvpEditForm(request.POST, instance=rsvp)
+            if form.is_valid():
+                rsvp_update = form.save(commit=False)
+                rsvp_update.save()
+                return redirect('event', pk=event.pk)
+        else:
+            form = RsvpEditForm(instance=rsvp)
+            return render(request, 'events/rsvp_edit.html', {'form': form, 'event':event})
