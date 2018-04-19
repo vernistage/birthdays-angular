445ec7449a922dcc5141aa076edc5fc055f00221
Sarah Colby <scolby@austin.utexas.edu>
2018-04-19 11:07:06 -0500

    initial test set-up

--- invitations/tests.py
+++ invitations/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
-
+from models import AppUser, Event, Rsvp
 # Create your tests here.
+
+class AppUserTestCase(TestCase):
+    def setUp(self):
+        pass
+
+class EventTestCase(TestCase):
+    def setUp(self):
+        pass
+
+class RsvpTestCase(TestCase):
+    def setUp(self):
+        pass
