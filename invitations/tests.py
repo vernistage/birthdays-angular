8b62f74125f239f7904879c57d94d1469e3bf74b
Sarah Colby <scolby@austin.utexas.edu>
2018-04-20 14:49:38 -0500

    finish relationship tests

--- invitations/tests.py
+++ invitations/tests.py
@@ -81,5 +81,37 @@ class EventTestCase(TestCase):
         self.assertEqual(str(test1_party.end_time), "2006-10-26 14:30:59.920830+00:00")
 
 class RsvpTestCase(TestCase):
-    def setUp(self):
-        pass
+    def test_invitation_properties(self):
+        test = AppUser.objects.create(
+                    first_name="Test",
+                    last_name="User",
+                    birth_date="2006-10-25",
+                    username="username",
+                    password="password"
+                )
+        test1 = AppUser.objects.create(
+                    first_name="Test1",
+                    last_name="User1",
+                    birth_date="2005-1-25",
+                    username="username1",
+                    password="password"
+                )
+        event = Event.objects.create(
+                    creator=AppUser.objects.get(username="username"),
+                    title="Test's Party",
+                    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                    address="123 Address St.",
+                    start_time="2006-10-25 14:30:59.920830+00:00",
+                    end_time="2006-10-26 14:30:59.920830+00:00"
+                )
+
+        rsvp = Rsvp.objects.create(
+            event=event,
+            invitee=test1,
+            is_attending=True
+        )
+
+        self.assertEqual(rsvp.event, event)
+        self.assertEqual(rsvp.invitee, test1)
+        self.assertTrue(rsvp.is_attending)
+        self.assertIn(test1, event.invitees.all())
