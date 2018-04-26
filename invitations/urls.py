4d72addb2532525c7c8aa5f85bc29b88412b8b36
Sarah Colby <scolby@austin.utexas.edu>
2018-04-26 12:49:14 -0500

    add authentication to urls

--- invitations/urls.py
+++ invitations/urls.py
@@ -1,12 +1,13 @@
 from . import views
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    url(r'^events/$', views.EventListView.as_view(), name='events'),
-    url(r'^events/(?P<pk>\d+)/$', views.EventDetailView.as_view(), name='event'),
-    url(r'^events/create/$', views.EventCreateView.as_view(), name='create_event'),
-    url(r'^events/edit/(?P<pk>\d+)/$', views.EventUpdateView.as_view(), name='update_event'),
-    url(r'^events/destroy/(?P<pk>\d+)/$', views.EventDeleteView.as_view(), name='destroy_event'),
-    url(r'^rsvps/$', views.RsvpListView.as_view(), name='rsvps'),
-    url(r'^rsvps/edit/(?P<pk>\d+)$', views.RsvpUpdateView.as_view(), name='update_rsvp'),
+    url(r'^events/$', login_required(views.EventListView.as_view()), name='events'),
+    url(r'^events/(?P<pk>\d+)/$', login_required(views.EventDetailView.as_view()), name='event'),
+    url(r'^events/create/$', login_required(views.EventCreateView.as_view()), name='create_event'),
+    url(r'^events/edit/(?P<pk>\d+)/$', login_required(views.EventUpdateView.as_view()), name='update_event'),
+    url(r'^events/destroy/(?P<pk>\d+)/$', login_required(views.EventDeleteView.as_view()), name='destroy_event'),
+    url(r'^rsvps/$', login_required(views.RsvpListView.as_view()), name='rsvps'),
+    url(r'^rsvps/edit/(?P<pk>\d+)$', login_required(views.RsvpUpdateView.as_view()), name='update_rsvp'),
 ]
