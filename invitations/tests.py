2e26090c15e247eee47189dbb654377d556a44f5
Sarah Colby <scolby@austin.utexas.edu>
2018-04-26 14:59:38 -0500

    format

--- invitations/tests.py
+++ invitations/tests.py
@@ -76,15 +76,17 @@ class HTTPLoggedOut(TestCase):
 
 class HTTPLoggedIn(TestCase):
     def setUp(self):
-        self.invitee = AppUser.objects.create(username='invitee',
-                                            password='password',
-                                            first_name='iam',
-                                            last_name='invited',
-                                            birth_date='1987-01-01')
-        self.user = AppUser.objects.create(username='testuser',
-                                            first_name='test',
-                                            last_name='user',
-                                            birth_date='1987-01-01')
+        self.invitee = AppUser.objects.create(
+            username='invitee',
+            password='password',
+            first_name='iam',
+            last_name='invited',
+            birth_date='1987-01-01')
+        self.user = AppUser.objects.create(
+            username='testuser',
+            first_name='test',
+            last_name='user',
+            birth_date='1987-01-01')
         self.client = Client()
         self.client.force_login(self.user, backend=None)
         self.event = Event.objects.create(
@@ -114,8 +116,6 @@ class HTTPLoggedIn(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "name")
         self.assertContains(response, "Hello")
-
-
 
     def test_logged_in_events(self):
         response = self.client.get(reverse('invitations:events'))
@@ -207,28 +207,27 @@ class EventTestCase(TestCase):
 class RsvpTestCase(TestCase):
     def test_invitation_properties(self):
         self.test = AppUser.objects.create(
-                    first_name="Test",
-                    last_name="User",
-                    birth_date="2006-10-25",
-                    username="username",
-                    password="password"
-                )
+            first_name="Test",
+            last_name="User",
+            birth_date="2006-10-25",
+            username="username",
+            password="password"
+        )
         self.test1 = AppUser.objects.create(
-                    first_name="Test1",
-                    last_name="User1",
-                    birth_date="2005-1-25",
-                    username="username1",
-                    password="password"
-                )
+            first_name="Test1",
+            last_name="User1",
+            birth_date="2005-1-25",
+            username="username1",
+            password="password"
+        )
         self.event = Event.objects.create(
-                    creator=AppUser.objects.get(username="username"),
-                    title="Test's Party",
-                    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-                    address="123 Address St.",
-                    start_time="2006-10-25 14:30:59.920830+00:00",
-                    end_time="2006-10-26 14:30:59.920830+00:00"
-                )
-
+            creator=AppUser.objects.get(username="username"),
+            title="Test's Party",
+            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+            address="123 Address St.",
+            start_time="2006-10-25 14:30:59.920830+00:00",
+            end_time="2006-10-26 14:30:59.920830+00:00"
+        )
         self.rsvp = Rsvp.objects.create(
             event=self.event,
             invitee=self.test1,
