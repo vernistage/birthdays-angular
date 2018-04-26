e4369c71dd6795c9ff54e85cd28a5194a73ddbb4
Sarah Colby <scolby@austin.utexas.edu>
2018-04-26 14:57:00 -0500

    DRY tests

--- invitations/tests.py
+++ invitations/tests.py
@@ -158,45 +158,36 @@ class HTTPLoggedIn(TestCase):
 # Model Tests
 class AppUserTestCase(TestCase):
     def setUp(self):
-        AppUser.objects.create(
+        self.test=AppUser.objects.create(
             first_name="Test",
             last_name="User",
             birth_date="2006-10-25",
             username="username",
             password="password")
-        AppUser.objects.create(
+        self.test1=AppUser.objects.create(
             first_name="Test1",
             last_name="User1",
-            birth_date="2005-1-25",
+            birth_date="2005-01-25",
             username="username1",
             password="password")
 
     def test_users_have_attributes(self):
-        test = AppUser.objects.get(username="username")
-        test1 = AppUser.objects.get(username="username1")
-
-        self.assertEqual(test.first_name, "Test")
-        self.assertEqual(test1.first_name, "Test1")
-        self.assertEqual(test.last_name, "User")
-        self.assertEqual(test1.last_name, "User1")
-        self.assertEqual(str(test.birth_date), '2006-10-25')
-        self.assertEqual(str(test1.birth_date), '2005-01-25')
+        self.assertEqual(self.test.first_name, "Test")
+        self.assertEqual(self.test1.first_name, "Test1")
+        self.assertEqual(self.test.last_name, "User")
+        self.assertEqual(self.test1.last_name, "User1")
+        self.assertEqual(str(self.test.birth_date), '2006-10-25')
+        self.assertEqual(str(self.test1.birth_date), '2005-01-25')
 
 class EventTestCase(TestCase):
     def setUp(self):
-        AppUser.objects.create(
+        self.test = AppUser.objects.create(
             first_name="Test",
             last_name="User",
             birth_date="2006-10-25",
             username="username",
             password="password")
-        AppUser.objects.create(
-            first_name="Test1",
-            last_name="User1",
-            birth_date="2005-1-25",
-            username="username1",
-            password="password")
-        Event.objects.create(
+        self.test_party = Event.objects.create(
             creator=AppUser.objects.get(username="username"),
             title="Test's Party",
             description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
@@ -204,51 +195,32 @@ class EventTestCase(TestCase):
             start_time="2006-10-25 14:30:59.920830+00:00",
             end_time="2006-10-26 14:30:59.920830+00:00"
         )
-        Event.objects.create(
-            creator=AppUser.objects.get(username="username1"),
-            title="Test 1's Party",
-            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-            address="321 Address St.",
-            start_time="2006-10-25 14:30:59.920830+00:00",
-            end_time="2006-10-26 14:30:59.920830+00:00"
-        )
 
     def test_events_have_attributes(self):
-        test = AppUser.objects.get(username="username")
-        test1 = AppUser.objects.get(username="username1")
-        test_party = Event.objects.get(title="Test's Party")
-        test1_party = Event.objects.get(title="Test 1's Party")
-
-        self.assertEqual(test_party.creator, test)
-        self.assertEqual(test1_party.creator, test1)
-        self.assertEqual(test_party.title, "Test's Party")
-        self.assertEqual(test1_party.title, "Test 1's Party")
-        self.assertEqual(test_party.address, "123 Address St.")
-        self.assertEqual(test1_party.address, "321 Address St.")
-        self.assertIn("Lorem ipsum dolor sit amet", test_party.description)
-        self.assertIn("Lorem ipsum dolor sit amet", test1_party.description)
-        self.assertEqual(str(test_party.start_time), "2006-10-25 14:30:59.920830+00:00")
-        self.assertEqual(str(test1_party.start_time), "2006-10-25 14:30:59.920830+00:00")
-        self.assertEqual(str(test_party.end_time), "2006-10-26 14:30:59.920830+00:00")
-        self.assertEqual(str(test1_party.end_time), "2006-10-26 14:30:59.920830+00:00")
+        self.assertEqual(self.test_party.creator, self.test)
+        self.assertEqual(self.test_party.title, "Test's Party")
+        self.assertEqual(self.test_party.address, "123 Address St.")
+        self.assertIn("Lorem ipsum dolor sit amet", self.test_party.description)
+        self.assertEqual(str(self.test_party.start_time), "2006-10-25 14:30:59.920830+00:00")
+        self.assertEqual(str(self.test_party.end_time), "2006-10-26 14:30:59.920830+00:00")
 
 class RsvpTestCase(TestCase):
     def test_invitation_properties(self):
-        test = AppUser.objects.create(
+        self.test = AppUser.objects.create(
                     first_name="Test",
                     last_name="User",
                     birth_date="2006-10-25",
                     username="username",
                     password="password"
                 )
-        test1 = AppUser.objects.create(
+        self.test1 = AppUser.objects.create(
                     first_name="Test1",
                     last_name="User1",
                     birth_date="2005-1-25",
                     username="username1",
                     password="password"
                 )
-        event = Event.objects.create(
+        self.event = Event.objects.create(
                     creator=AppUser.objects.get(username="username"),
                     title="Test's Party",
                     description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
@@ -257,13 +229,13 @@ class RsvpTestCase(TestCase):
                     end_time="2006-10-26 14:30:59.920830+00:00"
                 )
 
-        rsvp = Rsvp.objects.create(
-            event=event,
-            invitee=test1,
+        self.rsvp = Rsvp.objects.create(
+            event=self.event,
+            invitee=self.test1,
             is_attending=True
         )
 
-        self.assertEqual(rsvp.event, event)
-        self.assertEqual(rsvp.invitee, test1)
-        self.assertTrue(rsvp.is_attending)
-        self.assertIn(test1, event.invitees.all())
+        self.assertEqual(self.rsvp.event, self.event)
+        self.assertEqual(self.rsvp.invitee, self.test1)
+        self.assertTrue(self.rsvp.is_attending)
+        self.assertIn(self.test1, self.event.invitees.all())
