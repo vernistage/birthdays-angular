0a1e4cc33b533f44c643d0d393651ac897973edc
Sarah Colby <scolby@austin.utexas.edu>
2018-10-01 11:53:12 -0500

    initial angular set-up

--- angular-part/src/app/app.module.ts
+++ angular-part/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { EventsComponent } from './events/events.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule
