89d2a721356cad1cde53bd2d041e442363e8bf6b
Sarah Colby <scolby@austin.utexas.edu>
2018-10-01 15:43:06 -0500

    set up event class

--- angular-part/src/app/events/events.component.ts
+++ angular-part/src/app/events/events.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsComponent implements OnInit {
 
+  event = Event = {
+    id: 1,
+    title: "Event 1",
+    description: "First event",
+    address: "at the house",
+    start_time: "October 5",
+    end_time: "November 5",
+  };
+
   constructor() { }
 
   ngOnInit() {
